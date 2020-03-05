a8aec2e5b4534ef5d1c6dbe5283f3f0c58c70b73
garciacarlos78 <carles.garcia4@hotmail.com>
2020-03-05 22:53:55 +0100

    Initial commit

---
+++ src/main/java/com/cgrdev/springgettingstarted/Greeting.java
@@ -0,0 +1,14 @@
+package com.cgrdev.springgettingstarted;
+
+public class Greeting {
+
+    private final String name;
+
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
